25e7cec363b89b0390d0acd4597ce9ba758fcdc5
Viery Brian Fernanda_L200230231 <l200230231@student.ums.ac.id>
2025-12-15 20:23:14 +0700

    commit 2: add CVDocument layout

--- frontend/src/components/dashboard/Preview.jsx
+++ frontend/src/components/dashboard/Preview.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
+
+const CVDocument = React.forwardRef((props, ref) => (
+  <div ref={ref}>
+    <div className="grid grid-cols-12 min-h-[1120px]">
+      <div className="col-span-4 bg-blue-800 text-white p-8">
+        SIDEBAR
+      </div>
+      <div className="col-span-8 p-10">
+        CONTENT
+      </div>
+    </div>
+  </div>
+));
 
 export default function Preview() {
-  return (
-    <div>
-      <h2>Preview Profil & CV</h2>
-      <p>Isi biodata dulu.</p>
-    </div>
-  );
+  const componentRef = useRef();
+  return <CVDocument ref={componentRef} />;
 }
